6a76dafc4c62c03d62ab42b7976e1bbe20807575
Eemeli Aaltonen <69976749+poponappi@users.noreply.github.com>
2021-07-15 08:52:12 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -5,5 +5,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update
-  apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
